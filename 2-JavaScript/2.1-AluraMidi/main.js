0f176c0e5662ed5c1e26108959234ed62f127162
Adrians Fernandes <62452049+feradr@users.noreply.github.com>
2022-08-10 11:49:46 -0400

    Atualizando Arquivos

--- 2-JavaScript/2.1-AluraMidi/main.js
+++ 2-JavaScript/2.1-AluraMidi/main.js
@@ -1,11 +1,32 @@
-function TocaSom () {
-    document.querySelectorAll('.tecla').play();
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
-document.quer
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
 
-// Abaixo Codigo Antigo
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //Abaixo uma Template String em JS
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
+
+// Abaixo Codigo Antigo -------------------------
 // // Tim --------
 // function tocaSomPom () {
 //     document.querySelector('#som_tecla_pom').play();
